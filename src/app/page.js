153b6febfb89e4ae0478f0e77fa300f66c14e4be
Shreesh-Gawande <shreeshgawande10@gmail.com>
2024-06-28 20:16:42 +0530

    Homepage Completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,47 @@
 
-import Header from"@/components/layout/Header"
+
 import Hero from"@/components/layout/Hero"
 import HomeMenu from"@/components/layout/HomeMenu"
+import SectionHeaders from "@/components/layout/SectionHeaders";
 export default function Home() {
   return (
     <>
-     <Header/>
+     
      <Hero/>
      <HomeMenu/>
+     <section className="text-center my-16">
+      <SectionHeaders
+      subHeader="Our Story"
+      mainHeader="About Us"/>
+      <div className="flex flex-col gap-4 max-w-2xl max-auto mt-4 
+      text-gray-500  ">
+      <p  >
+        Lorem ipsum dolor, sit amet consectetur adipisicing 
+        elit. Recusandae nostrum totam pariatur aliquid! Vero
+         blanditiis quas dolore autem amet perferendis fugit 
+         officia consectetur 
+        aliquid sint commodi architecto, unde illum rerum!
+      </p>
+       <p >
+        Lorem ipsum dolor, sit amet consectetur adipisicing 
+        elit. Recusandae nostrum totam pariatur aliquid! Vero
+         blanditiis 
+         </p>
+         <p>quas dolore autem amet perferendis fugit 
+         officia consectetur 
+        aliquid sint commodi architecto, unde illum rerum!
+      </p>
+      </div>
+     </section>
+     <section className="text-center my-8">
+      <SectionHeaders
+      subHeader="Don't Hesitate"
+      mainHeader="Contact Us"/>
+<div className="mt-8">
+<a className="text-4xl underline text-gray-500 " href="+91123456789">+91123456789</a>
+</div>
+     </section>
+    
     </>
   );
 }
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -13,7 +13,12 @@ export default function Header() {
         <Link href="/menu">Menu</Link>
         <Link href="/about">About</Link>
         <Link href="/contact">Contact</Link>
-        <Link href="/login" className="bg-red-500 rounded-full text-white px-8 py-2">Login</Link>
+        
+      </nav>
+      <nav className="flex items-center gap-4 text-gray-500 font-semibold">
+        <Link href="/login">Login</Link>
+      <Link href="/register" className="bg-red-500 rounded-full text-white px-8 py-2">
+      Register</Link>
       </nav>
       </header></>
   )
