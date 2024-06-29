5c4d2f9e740381708e103546bc60c989ddf01d4c
Shreesh-Gawande <shreeshgawande10@gmail.com>
2024-06-29 12:43:25 +0530

    Register Page completed

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,5 +1,22 @@
+import Image from 'next/image';
 export default function RegisterPage (){
     return(
-        <div>Register</div>
+        <div>\
+            <section>
+                <h1 className="text-center text-red-500 
+                mb-6 text-4xl">
+                Register
+                </h1>
+            <form className="block max-w-xs mx-auto ">
+                <input type="email" placeholder="Email"/>
+                <input type="password" placeholder="Password"/>
+                <button type="submit">Register</button>
+                <div className="text-center my-4 text-gray-500">or login with provider</div>
+                <button className="flex gap-4 justify-center">
+                    <Image src="/google.png" alt="" width={24} height={24}/>
+                    Login with Google</button>
+            </form>
+            </section>
+        </div>
     )
 }
